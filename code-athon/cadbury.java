3bd5e965c19ca02006668dd0bf41f8c36b79d320
Divyanshu Kumar <divyanshukumarg@gmail.com>
2018-11-18 13:54:14 +0530

    working cadbury

--- code-athon/cadbury.java
+++ code-athon/cadbury.java
@@ -6,13 +6,44 @@
 import java.io.*;
 import java.util.*;
 public class cadbury {
+  static int a[][] = new int[201][201];
+  static int calc(int l, int w){
+    if(l==1){
+        a[l][w] = w;
+        return w;
+    }
+    if(w==1){
+        a[l][w] = l;
+        return l;
+    }
+    if(l<=0|| w<=0){
+      return 0;
+    }
+    if(a[l][w] != 0){
+        return a[l][w];
+    }
+    if(a[w][l] != 0){
+        return a[w][l];
+    }
+    int e = l;
+    l = l-w;
+    if(l==0){
+      return 1;
+    }
+    int u =l;
+    int v = w;
+    l = Math.max(u, v);
+    w = Math.min(u, v);
+    a[e][w] = 1 + calc(l, w);
+    return a[e][w];
+  }
     public static void main(String args[] ) throws Exception {
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
       int m = Integer.parseInt(br.readLine().trim());
       int n = Integer.parseInt(br.readLine().trim());
       int p = Integer.parseInt(br.readLine().trim());
       int q = Integer.parseInt(br.readLine().trim());
-      int a[][] = new int[201][201];
+
       int total = 0;
       for(int i = m; i<=n; i++){
           for(int j=p; j<=q; j++){
@@ -24,47 +55,9 @@ public class cadbury {
                   total +=a[j][i];
                   continue;
               }
-              int r = 0;
               int l = Math.max(i, j);
               int w = Math.min(i, j);
-              while(true){
-                  if(l==1){
-                      r += w;
-                      break;
-                  }
-                  if(w==1){
-                      r += l;
-                      break;
-                  }
-                  if(l<0|| w<0){
-                    break;
-                  }
-                  if(a[l][w] != 0){
-                      r+= a[l][w];
-                      l = l-w;
-                      l = Math.max(l, w);
-                      w = Math.min(l, w);
-                      continue;
-                  }
-                  if(a[w][l] != 0){
-                      r+= a[w][l];
-                      l = l-w;
-                      l = Math.max(l, w);
-                      w = Math.min(l, w);
-                      continue;
-                  }
-                  l = l-w;
-                  r++;
-                  if(l==0){
-                    break;
-                  }
-                  int u =l;
-                  int v = w;
-                  l = Math.max(u, v);
-                  w = Math.min(u, v);
-              }
-              a[i][j] = r;
-              total += r;
+              total += calc(l, w);
           }
       }
       System.out.println(""+total);
