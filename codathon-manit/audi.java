0e532f8a12578a364234c9670e4c5cb1e04ebc40
Divyanshu Kumar <divyanshukumarg@gmail.com>
2019-01-17 20:37:49 +0530

    2nd day full score

--- codathon-manit/audi.java
+++ codathon-manit/audi.java
@@ -2,17 +2,40 @@ import java.io.*;
 import java.util.*;
 
 public class audi{
+    static long power(long x, long y, long p)
+    {
+        // Initialize result
+        long res = 1;
+
+        // Update x if it is more
+        // than or equal to p
+        x = x % p;
+
+        while (y > 0)
+        {
+            // If y is odd, multiply x
+            // with result
+            if((y & 1)==1)
+                res = (res * x) % p;
+
+            // y must be even now
+            // y = y / 2
+            y = y >> 1;
+            x = (x * x) % p;
+        }
+        return res;
+    }
   public static void main(String args[] ) throws Exception {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
       int cases = Integer.parseInt(br.readLine().trim());
       for(int i=0; i<cases; i++){
         long n = Long.parseLong(br.readLine().trim());
-        long result = 0;
-        for(long j =2; j<=n; j++){
-          result = result + ((((j+1)%1000000007) * (Math.pow(2, (j-2))%1000000007))%1000000007);
-        }
-        result += 1;
-        System.out.println(""+result);
+        long result = power(2, n-1, 1000000007) * (n%1000000007);
+        result = result%1000000007;
+        bw.append(result+"\n");
       }
+      bw.flush();
+      bw.close();
     }
 }
