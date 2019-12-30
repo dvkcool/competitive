6d4c0af526957d063d608d8ccf22f938c1f13ad9
Divyanshu Kumar <divyanshukumarg@gmail.com>
2019-12-30 12:06:58 +0530

    bye bye 2019

--- codeforces/bye-2019/d.cpp
+++ codeforces/bye-2019/d.cpp
@@ -1,63 +1,21 @@
 #include <iostream>
-#include <vector>
-#include <iterator>
-#include <set>
-#include <algorithm>
+#include <map>
 using namespace std;
 int main(){
   ios_base::sync_with_stdio(false);
   cin.tie(0); cout.tie(0);
   int n, k, p, q, first = 0;
   cin>>n>>k;
-  set<int> query;
-  set<int>::iterator itr;
-  int a[n];
-  for(int i=0; i<n; i++) a[i] = -1;
-  for(int i=1; i<=k; i++){
-    query.insert(i);
-  }
-  int j = k;
-  for(int i=1; i<=n; i++){
-    if(j>n){
-      break;
-    }
+  map<int, int> count;
+  for(int i=0; i<=k; i++){
     cout<<"? ";
-    for(itr = query.begin(); itr!=query.end(); ++itr){
-      cout<<*itr<<" ";
+    for(int j=0; j<=k; j++){
+      if(i!=j) cout<<j+1<<" ";
     }
     cout.flush();
     cin>>p>>q;
-    a[p-1] = q;
-    query.erase(p);
-    j++;
-    query.insert(j);
+    count[q]++;
   }
-  cout<<"? ";
-
-  if(n-k+1 >=k){
-    int i = 1;
-    int j = 0;
-    vector<int> que;
-    while(i<=k && j<n){
-      if(a[j]!=-1){
-        cout<<j+1<<" ";
-        que.push_back(a[j]);
-        i++; j++;
-      }
-      else{
-        j++;
-      }
-    }
-    cout.flush();
-    cin>>p>>q;
-    sort(que.begin(), que.end());
-    for(i=0; i<k; i++){
-      //cout<<que[i]<<" ";
-      if(que[i]==q){
-        cout<<"! "<<i+1<<endl;
-      }
-    }
-  }
-
+  cout<<"! "<<k+1 - count.begin()->second<<endl;
   return 0;
 }
